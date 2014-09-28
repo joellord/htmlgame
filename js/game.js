ce701d510b87801f0ab4e1d819a301fc7288759d
Kae Bagg <kae.bagg@gmail.com>
2014-09-28 14:42:53 -0400

    Made a sprite object

--- js/game.js
+++ js/game.js
@@ -2,14 +2,6 @@ function init(){
 	imageLoader("img/sara-simple.png", gameLoop)
 };
 
-function imageLoader(src, callback){
-	img = new Image();
-	img.src = src;
-	img.onload = function(){
-		callback(img);
-	}
-};
-
 function gameLoop(){
 	canvas = document.getElementById("game");
 	context = canvas.getContext("2d");
@@ -17,5 +9,44 @@ function gameLoop(){
 	canvas.width = 500;
 	canvas.height = 500;
 
-	context.drawImage(img, 150, 150);
+	var sara = sprite({
+		context: context,
+		image: img,
+		width: 14,
+		height: 18
+		});
+
+	//context.drawImage(img, 14, 18);
+	sara.draw();
+};
+
+function sprite(params){
+	var sprite = {};
+	sprite.context = params.context;
+	sprite.image = params.image;
+	sprite.width = params.width;
+	sprite.height = params.height;
+
+	sprite.draw = function(){
+		sprite.context.drawImage(
+			sprite.image,
+			0,
+			0,
+			sprite.width,
+			sprite.height,
+			0,
+			0,
+			sprite.width,
+			sprite.height);
+	};
+
+	return sprite;
+}
+
+function imageLoader(src, callback){
+	img = new Image();
+	img.src = src;
+	img.onload = function(){
+		callback(img);
+	}
 };
