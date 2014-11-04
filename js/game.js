0139166b8e2381ba6aaa06da17d52138d484dbe1
Kae Bagg <kae.bagg@gmail.com>
2014-11-04 16:57:32 -0500

    Moved the sprite code to its own file

--- js/game.js
+++ js/game.js
@@ -52,7 +52,7 @@ function gameLoop(imgAssets){
 	}
 
 	function update(lastUpdated){
-		sara.update();
+		sara.update(keyPresses);
 	}
 
 	function render(){
@@ -64,128 +64,13 @@ function gameLoop(imgAssets){
 		sara.draw();
 	}
 
-	function sprite(params){
-		var sprite = {};
-		var frameIndex = 0;
-		var tickCount = 0;
-		var ticksPerFrame = ticksPerFrame || 3;
-		var numberOfFrames = params.numberOfFrames || 1;
-		sprite.context = params.context;
-		sprite.image = params.image;
-		sprite.width = params.width;
-		sprite.height = params.height;
-		sprite.positionX = params.positionX;
-		sprite.positionY = params.positionY;
-		sprite.loop = params.loop;
-		var newDirection = newDirection || params.direction; 
-
-		sprite.draw = function(){
-
-			if(newDirection == "Up"){
-				sprite.context.drawImage(
-					sprite.image,
-					frameIndex * sprite.width / numberOfFrames,
-					0,
-					sprite.width / numberOfFrames,
-					sprite.height,
-					sprite.positionX,
-					sprite.positionY,
-					sprite.width / numberOfFrames,
-					sprite.height);
-			} else if(newDirection == "Down"){
-				sprite.context.drawImage(
-					sprite.image,
-					frameIndex * sprite.width / numberOfFrames,
-					36,
-					sprite.width / numberOfFrames,
-					sprite.height,
-					sprite.positionX,
-					sprite.positionY,
-					sprite.width / numberOfFrames,
-					sprite.height);
-			} else if(newDirection == "Left"){
-				numberOfFrames = 3;
-				sprite.width = 47;
-
-				sprite.context.drawImage(
-					sprite.image,
-					frameIndex * sprite.width / numberOfFrames,
-					54,
-					sprite.width / numberOfFrames,
-					sprite.height,
-					sprite.positionX,
-					sprite.positionY,
-					sprite.width / numberOfFrames,
-					sprite.height);
-			} else if(newDirection == "Right"){
-				numberOfFrames = 3;
-				sprite.width = 47;
-
-				sprite.context.drawImage(
-					sprite.image,
-					frameIndex * sprite.width / numberOfFrames,
-					18,
-					sprite.width / numberOfFrames,
-					sprite.height,
-					sprite.positionX,
-					sprite.positionY,
-					sprite.width / numberOfFrames,
-					sprite.height);
-			}
-		};
-
-		sprite.update = function(){
-			switch (keyPresses.pop()){
-				case "Up":
-					sprite.positionY -= 3;
-					move();
-					newDirection = "Up"
-
-					break;
-				case "Left":
-					sprite.positionX -= 3;
-					move();
-					newDirection = "Left"
-					break;
-
-				case "Down":
-					sprite.positionY += 3;
-					move();
-					newDirection = "Down"
-					break;
-
-				case "Right":
-					sprite.positionX += 3;
-					move();
-					newDirection = "Right"
-					break;
-				default:
-					break;
-			}
-		};
-
-		function move(){
-			tickCount += 1;
-
-			if (tickCount > ticksPerFrame) {
-				tickCount = 0;
-
-				if(frameIndex < numberOfFrames - 1){
-					frameIndex += 1; 
-				} else if(sprite.loop){
-					frameIndex = 0;
-				}
-			}
-		};
-
-		return sprite;
-	}
+	
 
 	function nextFrame(){
 		var currentTime = new Date().getTime();
 
 		processInput(currentTime - lastUpdate);
-		update(currentTime - lastUpdate);
+		update();
 		render();
 
 		lastUpdate = currentTime;
@@ -207,3 +92,5 @@ function imageLoader(src, callback){
 
 	callback(imgAssets);
 };
+
+
