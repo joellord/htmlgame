f9905bcc760882a8b70e3bcb250c54d1c80c09b8
Kae Bagg <kae.bagg@gmail.com>
2014-11-19 22:29:16 -0500

    Retracing some steps

--- js/sprite.js
+++ js/sprite.js
@@ -1,116 +1,107 @@
 function sprite(params){
+	var frameIndex = 0;
+	var tickCount = 0;
+	var ticksPerFrame = ticksPerFrame || 3;
+	var numberOfFrames = params.numberOfFrames || 1;
 	this.context = params.context;
 	this.image = params.image;
 	this.width = params.width;
 	this.height = params.height;
 	this.positionX = params.positionX;
 	this.positionY = params.positionY;
-	this.loop = params.loop;
-	this.direction = params.direction; 
+	var loop = params.loop;
+	var newDirection = newDirection || params.direction;
 
-}
-
-sprite.prototype.frameIndex = 0;
-sprite.prototype.tickCount = 0;
-
-//Fix me
-sprite.prototype.ticksPerFrame = 3;
-sprite.prototype.numberOfFrames = 4;
-
-sprite.prototype.draw = function(){
-	if(this.direction == "Up"){
-		this.context.drawImage(
+	this.draw = function(){
+		if(newDirection == "Up"){
+			this.context.drawImage(
 			this.image,
-			this.frameIndex * this.width / this.numberOfFrames,
+			frameIndex * this.width / numberOfFrames,
 			0,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height,
 			this.positionX,
 			this.positionY,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height);
-	} else if(direction == "Down"){
-		this.context.drawImage(
+		} else if(newDirection == "Down"){
+			this.context.drawImage(
 			this.image,
-			this.frameIndex * this.width / this.numberOfFrames,
+			frameIndex * this.width / numberOfFrames,
 			36,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height,
 			this.positionX,
 			this.positionY,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height);
-	} else if(direction == "Left"){
-		this.this.numberOfFrames = 3;
-		this.width = 47;
+		} else if(newDirection == "Left"){
+			numberOfFrames = 3;
+			this.width = 47;
 
-		this.context.drawImage(
+			this.context.drawImage(
 			this.image,
-			this.frameIndex * this.width / this.numberOfFrames,
+			frameIndex * this.width / numberOfFrames,
 			54,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height,
 			this.positionX,
 			this.positionY,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height);
-	} else if(direction == "Right"){
-		this.numberOfFrames = 3;
-		this.width = 47;
+		} else if(newDirection == "Right"){
+			numberOfFrames = 3;
+			this.width = 47;
 
-		this.context.drawImage(
+			this.context.drawImage(
 			this.image,
-			this.frameIndex * this.width / this.numberOfFrames,
+			frameIndex * this.width / numberOfFrames,
 			18,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height,
 			this.positionX,
 			this.positionY,
-			this.width / this.numberOfFrames,
+			this.width / numberOfFrames,
 			this.height);
-	}
-};
-
-sprite.prototype.update = function(keyPresses){
-	switch (keyPresses.pop()){
-		case "Up":
-			this.positionY -= 3;
-			move();
-			newDirection = "Up"
-
-			break;
-		case "Left":
-			this.positionX -= 3;
-			move();
-			newDirection = "Left"
-			break;
-
-		case "Down":
-			this.positionY += 3;
-			move();
-			newDirection = "Down"
-			break;
-
-		case "Right":
-			this.positionX += 3;
-			move();
-			newDirection = "Right"
-			break;
-		default:
-			break;
-	}
-};
-
-function move(){
-	this.tickCount += 1;
-
-	if (this.tickCount > this.ticksPerFrame) {
-		this.tickCount = 0;
-
-		if(this.frameIndex < this.numberOfFrames - 1){
-			this.frameIndex += 1; 
-		} else if(this.loop){
-			this.frameIndex = 0;
 		}
-	}
-};
+	};
+
+	this.update = function(keyPresses){
+		switch (keyPresses.pop()){
+			case "Up":
+				this.positionY -= 3;
+				move();
+				newDirection = "Up"
+			break;
+			case "Left":
+				this.positionX -= 3;
+				move();
+				newDirection = "Left"
+			break;
+			case "Down":
+				this.positionY += 3;
+				move();
+				newDirection = "Down"
+			break;
+			case "Right":
+				this.positionX += 3;
+				move();
+				newDirection = "Right"
+			break;
+			default:
+			break;
+		}
+	};
+	function move(){
+		tickCount += 1;
+		if (tickCount > ticksPerFrame) {
+			tickCount = 0;
+			if(frameIndex < numberOfFrames - 1){
+				frameIndex += 1;
+			} else if(loop){
+				frameIndex = 0;
+			}
+
+		}
+	};
+}
