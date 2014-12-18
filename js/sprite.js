9101ebaf92c27d492bbe1ff8b5f72d8b94df9aa0
Kae Bagg <ThereExistsX@users.noreply.github.com>
2014-12-18 15:35:29 -0500

    Next time, remind me never to make a JS game

--- js/sprite.js
+++ js/sprite.js
@@ -46,30 +46,6 @@ sprite.prototype.draw = function(context) {
 						this.height);
 };
 
-sprite.prototype.update = function(keyPresses){
-	switch(keyPresses.pop()){
-		case "Up":
-			this.direction = "Up"
-			this.positionY -= 3;
-			this.moving = true;
-		break;
-		case "Left":
-			this.direction = "Left"
-			this.positionX -= 3;
-			this.moving = true;
-		break;
-		case "Right":
-			this.direction = "Right"
-			this.positionX += 3;
-			this.moving = true;
-		break;
-		case "Down":
-			this.direction = "Down"
-			this.positionY += 3;
-			this.moving = true;
-		break;
-		default:
-			this.moving = false;
-		break;
-	}
+sprite.prototype.update = function(){
+	//soon
 }
